f2ec17474b367b6129f0f71c904c3484d2d83f15
Xvfzy <3241729816zlj@gmail.com>
2025-10-22 23:51:22 +0800

    fix: adjust HTML rendering method

--- web/src/components/Directory.vue
+++ web/src/components/Directory.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+
+import { ref, watch, h } from 'vue'
+
+import { NAnchor, NAnchorLink } from 'naive-ui'
+import type { VNode } from 'vue'
 
 const props = defineProps<{
     target: HTMLElement | null
@@ -13,17 +17,30 @@ interface DirectoryItem {
 }
 
 let map = new Map<number, number>()
+
 const directory = ref<DirectoryItem[]>([])
+
 let index = 0
+
 let stack: DirectoryItem[] = []
 
 watch(() => props.target, (newVal) => {
-    if (!newVal) return
+
+    directory.value = []
+    stack = []
+    index = 0
+    map.clear()
+
+    if (!newVal) {
+        return
+    }
 
     const headings = newVal?.querySelectorAll('h1, h2, h3, h4, h5, h6') as NodeListOf<HTMLHeadingElement> | null
 
     headings?.forEach((item: HTMLHeadingElement) => {
+
         const level = Number(item.tagName.substring(1))
+
         const currentObj: DirectoryItem = {
             tag: item.tagName,
             name: item.innerText,
@@ -46,35 +63,35 @@ watch(() => props.target, (newVal) => {
         // 这个记录是因为目前naive  ui的侧边导航需要href跳转（导致标题标签需要加id），而不是scrollTo定位，万一后续需要用scrollTo定位的话，这个map就派上用场了
         map.set(index, item.getBoundingClientRect().top + window.scrollY)
     })
+
+    
 })
+
+const renderAnchorLink = (items: DirectoryItem[]): VNode[] => {
+        return items.map((item) =>
+            h(
+                NAnchorLink,
+                { href: '#heading-' + item.index, title: item.name },
+                item.children.length > 0 ? () => renderAnchorLink(item.children) : []
+            )
+        )
+    }
+
+    const renderDirectory = {
+        render() {
+            return renderAnchorLink(directory.value)
+        }
+    }
+
 </script>
 
 <template>
-    <div class="directory">
+    <div>
         <h2>目录</h2>
         <n-anchor show-rail show-background>
-            <!-- h1 -->
-            <n-anchor-link v-for="item in directory" :key="item.index" :href="'#heading-' + item.index" :title="item.name">
-                <!-- h2 -->
-                <n-anchor-link v-for="child in item.children" :key="child.index" :href="'#heading-' + child.index"
-                    :title="child.name">
-                    <!-- h3 -->
-                    <n-anchor-link v-for="grandChild in child.children" :key="grandChild.index"
-                        :href="'#heading-' + grandChild.index" :title="grandChild.name">
-                        <!-- h4 -->
-                        <n-anchor-link v-for="h4 in grandChild.children" :key="h4.index" :href="'#heading-' + h4.index"
-                            :title="h4.name">
-                            <!-- h5 -->
-                            <n-anchor-link v-for="h5 in h4.children" :key="h5.index" :href="'#heading-' + h5.index"
-                                :title="h5.name">
-                                <!-- h6 -->
-                                <n-anchor-link v-for="h6 in h5.children" :key="h6.index" :href="'#heading-' + h6.index"
-                                    :title="h6.name" />
-                            </n-anchor-link>
-                        </n-anchor-link>
-                    </n-anchor-link>
-                </n-anchor-link>
-            </n-anchor-link>
+            <component :is="renderDirectory" />
         </n-anchor>
     </div>
 </template>
+
+<style scoped></style>
